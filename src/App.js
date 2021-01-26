6607a899db80bbae567247a470f0cfb00ea6cbbf
Raymund Caringal <rcrock1978@gmail.com>
2021-01-26 14:07:09 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
